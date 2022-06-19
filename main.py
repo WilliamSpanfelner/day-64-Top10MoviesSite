2c2b8c23edd607f5e3abb1fca0bcedbac02802ef
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-06-19 21:31:25 +0300

    Pass db records across to the index.html document.
    Prevent Integrity Error by 'commenting' the new record creation. Comment the db creation line also since there is no need to create again.

--- main.py
+++ main.py
@@ -26,24 +26,25 @@ class Movie(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-db.create_all()
-
-new_movie = Movie(
-    title="Phone Booth",
-    year="2002",
-    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-    rating=7.3,
-    ranking=10,
-    review="My favourite character was the caller.",
-    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-)
-db.session.add(new_movie)
-db.session.commit()
+# db.create_all()
+#
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year="2002",
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# db.session.add(new_movie)
+# db.session.commit()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = Movie.query.all()
+    return render_template("index.html", movies=movies)
 
 
 if __name__ == '__main__':
