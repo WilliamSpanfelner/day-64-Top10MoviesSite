35d8cb9c8a19b8d68edc04df829dc8af25d36ab8
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-06-20 16:58:09 +0300

    Keep form fields as StringFields and
    perform conversion where necessary.

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, FloatField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class RateMovieForm(FlaskForm):
-    new_rating = FloatField('Your Rating Out of 10 e.g. 7.5', validators=[DataRequired()])
+    new_rating = StringField('Your Rating Out of 10 e.g. 7.5', validators=[DataRequired()])
     new_review = StringField('Your Review', validators=[DataRequired()])
     submit = SubmitField('Done')
