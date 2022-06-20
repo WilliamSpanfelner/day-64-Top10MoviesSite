78739c1aca3a4903e64d015024963ce71cc9ae19
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-06-20 20:34:03 +0300

    Create the AddMovieForm object.

--- forms.py
+++ forms.py
@@ -3,6 +3,11 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
+class AddMovieForm(FlaskForm):
+    title = StringField('Movie Title', validators=[DataRequired()])
+    submit = SubmitField('Add Movie')
+
+
 class RateMovieForm(FlaskForm):
     new_rating = StringField('Your Rating Out of 10 e.g. 7.5', validators=[DataRequired()])
     new_review = StringField('Your Review', validators=[DataRequired()])
